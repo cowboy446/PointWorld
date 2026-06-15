e7bf99124b74d25cbdce1b27ee9e4cf31f389644
zhangrong <3174848262@qq.com>
2026-06-15 05:07:08 +0000

    config minimum training(droid_1190clips)

---
+++ train.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0 
+# data_dirs必须是绝对路径！不然就会默认从data_components里开始拼相对路径，而不是根目录
+python train.py \
+  --domains=droid \
+  --ptv3_size=base \
+  --data_dirs=/backup/zhangrong/data/workspace/robot-wm/point-wm/PointWorld/restore_data/pointworld_droid_subset_restored/droid/wds \
+  --norm_stats_path=stats/droid \
+  --batch_size=22 \
+  --eval_freq=300 \
+  --save_freq=300 \
+  --num_workers=16 \
+  --eval_num_workers=5 \
+  --eval_freq=-1 \
+  --exp_name=pointworld_pretrain_droid_1190clips
