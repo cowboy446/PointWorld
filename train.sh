a8d483ae2235702b732c82cd6b4bd4a106353778
zhangrong <3174848262@qq.com>
2026-06-16 13:01:11 +0000

    Update training script to support multi-GPU training and adjust data directory

--- train.sh
+++ train.sh
@@ -1,9 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 
+# base --> outdim=256, small --> outdim=128,
 # data_dirs必须是绝对路径！不然就会默认从data_components里开始拼相对路径，而不是根目录
-python train.py \
+CUDA_VISIBLE_DEVICES=0,4 python train.py \
   --domains=droid \
   --ptv3_size=base \
-  --data_dirs=/backup/zhangrong/data/workspace/robot-wm/point-wm/PointWorld/restore_data/pointworld_droid_subset_restored/droid/wds \
+  --predictor_dim=256 \
+  --data_dirs=/backup/zhangrong/data/workspace/robot-wm/point-wm/PointWorld/restore_data/pointworld_droid_subset_restored/droid/wds_8152clips \
   --norm_stats_path=stats/droid \
   --batch_size=22 \
   --eval_freq=300 \
@@ -11,4 +12,5 @@ python train.py \
   --num_workers=16 \
   --eval_num_workers=5 \
   --eval_freq=-1 \
-  --exp_name=pointworld_pretrain_droid_1190clips
+  --exp_name=pointworld_pretrain_droid_8152clips_ddp2gpu \
+  --distributed True 
