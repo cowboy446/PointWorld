4d96289f605947e219030d86480908df36c1cb96
digital_zr <3174848262@qq.com>
2026-06-18 20:16:33 +0800

    Update ablation training script

--- train.sh
+++ train.sh
@@ -1,16 +1,17 @@
 # base --> outdim=256, small --> outdim=128,
 # data_dirs必须是绝对路径！不然就会默认从data_components里开始拼相对路径，而不是根目录
-CUDA_VISIBLE_DEVICES=0,4 python train.py \
+CUDA_VISIBLE_DEVICES=0 python train.py \
   --domains=droid \
-  --ptv3_size=base \
-  --predictor_dim=256 \
-  --data_dirs=/backup/zhangrong/data/workspace/robot-wm/point-wm/PointWorld/restore_data/pointworld_droid_subset_restored/droid/wds_8152clips \
+  --ptv3_size=small \
+  --predictor_dim=128 \
+  --data_dirs=/home/zhangrong/zhangrong-workspace/robot-wm/point-wm/PointWorld/restore_data/pointworld_droid_subset_restored_test_shard10/droid/wds \
   --norm_stats_path=stats/droid \
-  --batch_size=22 \
-  --eval_freq=300 \
-  --save_freq=300 \
+  --batch_size=1 \
+  --eval_freq=30 \
+  --save_freq=30 \
   --num_workers=16 \
   --eval_num_workers=5 \
-  --eval_freq=-1 \
-  --exp_name=pointworld_pretrain_droid_8152clips_ddp2gpu \
-  --distributed True 
+  --exp_name=pointworld_pretrain_droid_shard10_dino_11_23_layer \
+  --robot_use_gripper_open_feature=true \
+  --scene_use_dino=true \
+  --scene_dino_layers=11,23
